0a99547eb921ff90a9b0d4b7eaa5d803f30c7c52
Тинитилов Дмитрий Анатольевич <Tinitilov@od.itstep.local>
2018-09-10 18:53:45 +0300

    express

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 var http = require('http');
 var url  = require('url');
 
-var server = http.createServer(function(req, res) {
-	var parts = url.parse(req.url,true);
-	res.writeHead(200);
-	if (parts.query && parts.query.a && parts.query.b) {
-		var sum = +parts.query.a + +parts.query.b;
+
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+
+	if (req.query && req.query.a && req.query.b) {
+		var sum = +req.query.a + +req.query.b;
 		res.end(sum+'');
 	}
 	else
   		res.end('No parameters were found');
-});
+})
 
-server.listen(8080,function(){
-	console.log('Server listen on port 8080')
-});
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Server listening at http://%s:%s", host, port)
+
+})	
